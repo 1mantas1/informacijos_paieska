2c5891cd530d81e7a6e6c8400727430744dd778e
1mantas1 <31568005+1mantas1@users.noreply.github.com>
2020-11-22 21:38:22 +0200

    Korekcijos ir darbas su lab3

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,60 +32,122 @@
             this.button1 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.infoBottomLabel = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(28, 12);
+            this.textBox1.Location = new System.Drawing.Point(260, 21);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(896, 22);
+            this.textBox1.Size = new System.Drawing.Size(512, 22);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(307, 40);
+            this.button1.Location = new System.Drawing.Point(318, 49);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(163, 33);
+            this.button1.Size = new System.Drawing.Size(111, 33);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Ieškoti";
+            this.button1.Text = "Ieškoti LAB2";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.richTextBox1.Location = new System.Drawing.Point(0, 79);
+            this.richTextBox1.Location = new System.Drawing.Point(0, 106);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(936, 438);
+            this.richTextBox1.Size = new System.Drawing.Size(982, 601);
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(476, 40);
+            this.button2.Location = new System.Drawing.Point(170, 21);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(163, 33);
+            this.button2.Size = new System.Drawing.Size(84, 29);
             this.button2.TabIndex = 3;
             this.button2.Text = "Išvalyti";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.infoBottomLabel);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Location = new System.Drawing.Point(0, 713);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(982, 40);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Info";
+            // 
+            // infoBottomLabel
+            // 
+            this.infoBottomLabel.AutoSize = true;
+            this.infoBottomLabel.Location = new System.Drawing.Point(28, 17);
+            this.infoBottomLabel.Name = "infoBottomLabel";
+            this.infoBottomLabel.Size = new System.Drawing.Size(0, 17);
+            this.infoBottomLabel.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(982, 100);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(448, 49);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 33);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Ieškoti LAB3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(581, 49);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(111, 33);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Ieškoti LAB4";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(936, 517);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(982, 753);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1000, 800);
+            this.MinimumSize = new System.Drawing.Size(1000, 800);
             this.Name = "Form1";
-            this.Text = "Manto paieška";
+            this.Text = "Manto paieška Lab1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -95,6 +157,11 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label infoBottomLabel;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LemmaSharp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,6 +10,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using XLemmatizer;
 
 namespace informacijos_paieska
 {
@@ -18,8 +20,65 @@ namespace informacijos_paieska
         {
             InitializeComponent();
         }
+        // static ILemmatizer lmtz = new LemmatizerPrebuiltCompact(LanguagePrebuilt.English);
+
+        public class Turinys
+        {
+            public int failoIndeksas { get; set; }
+            public string failoTurinys { get; set; }
+            public string[] zodis { get; set; }
+            public int[] zodziaiCount { get; set; }
+
+            public Turinys()
+            {
+
+            }
+            public void darbasSuTuriniu()
+            {
+                string[] turinioZodziai = failoTurinys.Split(' ', '\n', ' ', '.', ',', '-', '?', '|', '✓', '!', '/', ':', '\'', '\"', '(', ')', ';');
+                turinioZodziai = turinioZodziai.Where(x => !string.IsNullOrWhiteSpace(x))
+                                 .ToArray();
+                //MessageBox.Show("Yra " + turinioZodziai.Length.ToString() + " Žodžių faile");
+                //Surenkami visi unikalūs žodžiai
+                for (int i = 0; i < turinioZodziai.Length; i++)
+                {
+                    turinioZodziai[i] = turinioZodziai[i].Trim().ToLower();
+                }
+                this.zodis = turinioZodziai.Distinct().ToArray();
+                zodziaiCount = new int[zodis.Length];
+                //MessageBox.Show("Yra " + zodis.Length.ToString() + " uniklaių faile");
+                //suskaičiuojama kiek kiekvienas žodis pasikartoja faile
+
+                zodis = zodis.Except(new List<string> { string.Empty }).ToArray();
+
+                for (int i = 0; i < zodis.Length; i++)
+                {
+                    for (int j = 0; j < turinioZodziai.Length; j++)
+                    {
+                        if (zodis[i] == turinioZodziai[j])
+                        {
+                            zodziaiCount[i]++;
+                        }
+                    }
+                }
+            }
+            Lemmatizer lemmatizer = new Lemmatizer();
+            public void listWords(RichTextBox ats)
+            {
+                
+                darbasSuTuriniu();
+                for (int i = 0; i < zodis.Length; i++)
+                {
+                    ats.Text += zodis[i] + " ; " + zodziaiCount[i] + " ; " + lemmatizer.(zodis[i]) + Environment.NewLine;
+                }
+
+            }
+        }
+
         public string[] turinys = new string[30];
         public string[] failu_indeksai = new string[30];
+
+        List<Turinys> failai = new List<Turinys>();
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -29,20 +88,27 @@ namespace informacijos_paieska
             {
                 failu_indeksai[fileIndex] = filesNames;
                 turinys[fileIndex] = File.ReadAllText(filesNames);
+                failai.Add(new Turinys() { failoIndeksas = fileIndex, failoTurinys = turinys[fileIndex] });
+                failai.ElementAt(fileIndex).listWords(richTextBox1);
                 fileIndex++;
-                
             }
+            infoBottomLabel.Text = "Failai Nustaikyti";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-          richTextBox1.Clear();
-          string paieskosZodziai = textBox1.Text;
-          string[] ieskotiZodziu = Regex.Split(paieskosZodziai,"and");
-          Paieska(ieskotiZodziu);
+            richTextBox1.Clear();
+            string paieskosZodziai = textBox1.Text;
+            string[] ieskotiZodziu = Regex.Split(paieskosZodziai, " ");
+            for (int i = 0; i < ieskotiZodziu.Length; i++)
+            {
+                ieskotiZodziu[i] = ieskotiZodziu[i].Trim();
+            }
+            // Paieska(ieskotiZodziu);
         }
 
-        public void Paieska(string[] searchWords){
+        public void Paieska(string[] searchWords)
+        {
             for (int i = 0; i < turinys.Length; i++)
             {
                 if (turinys[i] != null)
@@ -54,14 +120,15 @@ namespace informacijos_paieska
                             richTextBox1.Text += word + "[" + Regex.Matches(turinys[i], word).Count + "]" + " ";
                     }
                 }
-
             }
         }
+
 
         private void button2_Click(object sender, EventArgs e)
         {
             textBox1.Clear();
             richTextBox1.Clear();
         }
+
     }
 }
